ba1dbf0d10c4dd248077721b34c6f95034a7d30b
tekofx <tekofxx@gmail.com>
2022-02-21 18:13:58 +0100

    Created class pomopy

--- pomopy/pomo.py
+++ pomopy/pomo.py
@@ -10,33 +10,36 @@ LONG_BREAK_TIME = 30 * 60
 console = Console()
 
 
-def progress_bar(time: int, description: str):
-    for step in track(range(time), description=description):
-        sleep(1)
-
-
-count = 0
-while True:
-
-    if count != 3:
-        os.system("clear")
-        progress_bar(WORK_TIME, "Working")
-        os.system("clear")
-        count += 1
-        console.print("Working time has ended, would you like to take a break?: y/n")
-        var = input()
-        os.system("clear")
-        if var == "y":
-            progress_bar(SHORT_BREAK_TIME, "Break!")
-            os.system("clear")
-            console.print(
-                "Break has ended, press enter when you are ready to work again"
-            )
-            var = input()
-    else:
-        console.print(
-            "Congratulations! You have completed 4 pomodoros, press enter to start long break"
-        )
-        var = input()
-        progress_bar(LONG_BREAK_TIME, "Long Break!")
+class pomopy:
+    def __init__(self) -> None:
         count = 0
+        while True:
+
+            if count != 3:
+                os.system("clear")
+                self.progress_bar(WORK_TIME, "Working")
+                os.system("clear")
+                count += 1
+                console.print(
+                    "Working time has ended, would you like to take a break?: y/n"
+                )
+                var = input()
+                os.system("clear")
+                if var == "y":
+                    self.progress_bar(SHORT_BREAK_TIME, "Break!")
+                    os.system("clear")
+                    console.print(
+                        "Break has ended, press enter when you are ready to work again"
+                    )
+                    input()
+            else:
+                console.print(
+                    "Congratulations! You have completed 4 pomodoros, press enter to start long break"
+                )
+                input()
+                self.progress_bar(LONG_BREAK_TIME, "Long Break!")
+                count = 0
+
+    def progress_bar(self, time: int, description: str):
+        for _ in track(range(time), description=description):
+            sleep(1)
